2e6bbe7495cf16cb797cddc3369566eee3e55f91
Hephzibah S <142471663+Hephzibah811@users.noreply.github.com>
2024-02-12 09:39:34 +0530

    Delete backend/src directory

--- backend/src/main/java/com/bms/repo/AccountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bms.model.Account;
-
-public interface AccountRepo extends JpaRepository<Account, Long>{
-	
-}
--- backend/src/main/java/com/bms/repo/TransactionRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bms.model.Transaction;
-
-public interface TransactionRepo extends JpaRepository<Transaction, Long>{
-}
